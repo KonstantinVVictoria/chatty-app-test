23bb9b308496942735065a2a257de19a3ea06407
KonstantinTheRed <61480260+KonstantinTheRed@users.noreply.github.com>
2020-07-19 09:35:41 -0700

    fixed issue with connecting to server

--- client/src/components/ChatBar.jsx
+++ client/src/components/ChatBar.jsx
@@ -5,20 +5,36 @@ class ChatBar extends Component {
   state = {};
   render() {
     return (
-      <div className="chatbar-container">
+      <div
+        className="chatbar-container"
+        style={{
+          height: window.vH * 80 * 0.1 + "px",
+          width: window.vW * 40 + "px",
+        }}
+      >
         <div
           className="center-completely"
-          style={{ width: "90%", height: "70%", margin: "0 auto" }}
+          style={{
+            width: window.vW * 40 * 0.9 + "px",
+            height: window.vH * 80 * 0.7 * 0.1 + "px",
+            margin: "0 auto",
+          }}
         >
           <input
             className="chatbar-input"
             style={{
+              width: window.vW * 40 * 0.9 + "px",
+              height: window.vH * 80 * 0.7 * 0.1 + "px",
               padding: `0px ${window.vH * 40 * 0.1 * 0.9 * 0.5}px 0px`,
               fontSize: 3 * window.vH + "px",
             }}
           />
           <button
             className="chatbar-send"
+            style={{
+              width: window.vW * 40 * 0.9 * 0.1 + "px",
+              height: window.vH * 80 * 0.7 * 0.1 + "px",
+            }}
             onMouseDown={() => {
               this.props.sendMessage(
                 document.getElementsByClassName("chatbar-input")[0].value
